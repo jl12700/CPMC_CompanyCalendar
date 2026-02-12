55f6023f47ac1d99fc3bdb4938761bf68227fddd
jl12700 <john_lloyd_veneracion@dlsl.edu.ph>
2026-02-12 10:43:38 +0800

    fix vercel issue

--- src/components/layout/UserLayout.jsx
+++ src/components/layout/UserLayout.jsx
@@ -7,7 +7,7 @@ export default function UserLayout({ children }) {
       <UserSidebar />
       <UserTopBar />
       <main className="ml-56 mt-16 p-6">
-        <div className="max-w-6xl mx-auto">{children}</div>
+        <div className="max-w-7xl mx-auto">{children}</div>
       </main>
     </div>
   );
